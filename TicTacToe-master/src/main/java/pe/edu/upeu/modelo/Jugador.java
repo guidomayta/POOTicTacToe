93ce51194c11394d424de3675635fbc16cd97266
guidomayta <guido1808daniel2006@gmail.com>
2024-09-20 08:34:23 -0500

    Add files via upload

---
+++ TicTacToe-master/src/main/java/pe/edu/upeu/modelo/Jugador.java
@@ -0,0 +1,36 @@
+package pe.edu.upeu.modelo;
+
+public class Jugador {
+
+    private String nombre;
+    private String simbolo;
+
+    public Jugador(String nombre, String simbolo) {
+        this.nombre = nombre;
+        this.simbolo = simbolo;
+    }
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.simbolo = "X";
+    }
+    public Jugador( ) {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+}
